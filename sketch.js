fb9619e3ea03ca92526bcc7e9400aa68382a248e
Suzanna Neely Yates <nyates@students.pitzer.edu>
2021-09-22 10:35:35 -0700

    Co-authored-by: John Church <jchurch21@students.claremontmckenna.edu>

--- sketch.js
+++ sketch.js
@@ -17,6 +17,7 @@ function setup() {
 
     // Hide the video element, and just show the canvas
     video.hide();
+
 }
 
 function modelReady() {
@@ -24,10 +25,65 @@ function modelReady() {
 }
 
 function draw() {
-    image(video, 0, 0, width, height);
+    // flip camera
+    translate(video.width, 0);
+    scale(-1, 1);
+    image(video, 0, 0);
+
+    // create gradient
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+
+    //var grd = ctx.createLinearGradient(0, 0, 170, 0);
+    var grd = ctx.createLinearGradient(0, 0, 630, 0);
+    grd.addColorStop(0, "blue");
+    grd.addColorStop(.5, "purple");
+    grd.addColorStop(1, "red");
+
+    ctx.fillStyle = grd;
+    //ctx.fillRect(20, 20, 150, 100);
+    ctx.fillRect(20, 20, 610, 100);
 
     // We can call both functions to draw all keypoints and the skeletons
     drawKeypoints();
+
+
+
+    let cordsX = []
+    let cordsY = []
+
+
+    for (let i = 0; i < predictions.length; i += 1) {
+        const prediction = predictions[i];
+        for (let j = 0; j < prediction.landmarks.length; j += 1) {
+            const keypoint = prediction.landmarks[j];
+            cordsX.push(keypoint[0]);
+            cordsY.push(keypoint[1]);
+        }
+    }
+    console.log("Cords")
+    console.log(cordsX)
+    let avgX = 0;
+    let avgY = 0;
+    for (let i = 0; i < cordsX.length; i += 1) {
+        avgX = avgX + cordsX[i];
+        avgY = avgY + cordsY[i];
+    }
+    avgX = avgX / cordsX.length;
+    avgY = avgY / cordsX.length;
+    console.log("avg")
+    console.log(avgX)
+    let varianceX = 0;
+    let varianceY = 0;
+    for (let i = 0; i < cordsX.length; i += 1) {
+        varianceX += cordsX[i] - avgX;
+        varianceY += cordsY[i] - avgY;
+    }
+    varianceX = (varianceX / cordsX.length);
+    varianceY = (varianceY / cordsY.length);
+    console.log("variance")
+    console.log(varianceX);
+
 }
 
 // A function to draw ellipses over the detected keypoints
